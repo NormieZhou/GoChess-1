b6972a09a541c948a7c4d2f0a535c2ee19d8b6b6
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-14 16:18:05 -0700

    Board.h

---
+++ Board.h
@@ -0,0 +1,37 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <stdbool.h>
+#include "Player.h"
+
+// Piece type enumeration definition
+enum PieceType {
+    KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN
+};
+
+struct Position {
+    // Horizontal coordinate
+    int x;
+    // Vertical coordinate
+    int y;
+};
+
+struct Move {
+    // Starting position
+    struct Position start;
+    // Ending position
+    struct Position end;
+};
+
+struct Board {
+    // Two-dimensional array representing pieces on each position of the board
+    struct Piece *squares[8][8];
+};
+
+// Function to initialize the board
+void init_board(struct Board *board);
+
+// Function to copy the board
+void copy_board(struct Board *src, struct Board *dest);
+
+#endif /* BOARD_H */
