28817aade19592c9fdd2b30797d67b17d3877578
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-14 16:15:45 -0700

    Log.c

---
+++ Log.c
@@ -0,0 +1,16 @@
+#include "Log.h"
+
+void init_board_list(struct BoardList *list) {
+}
+
+void insert_board(struct BoardList *list, struct Board *board) {
+}
+
+void delete_board(struct BoardList *list) {
+}
+
+void clear_board_list(struct BoardList *list) {
+}
+
+void undo_move(struct Game *game, struct BoardList *list) {
+}
