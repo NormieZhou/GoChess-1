b3561b179e6fcb1b6dc139413ae84880f8eda741
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-14 16:14:51 -0700

    CheckMove.c

---
+++ CheckMove.c
@@ -0,0 +1,22 @@
+#include "CheckMove.h"
+
+bool check_move_pawn(struct Piece *board[8][8], int x, int y) {
+}
+
+bool check_move_knight(struct Piece *board[8][8], int x, int y){
+}
+
+bool check_move_bishop(struct Piece *board[8][8], int x, int y){
+}
+
+bool check_move_rook(struct Piece *board[8][8], int x, int y){
+}
+
+bool check_move_queen(struct Piece *board[8][8], int x, int y){
+}
+
+bool check_move_king(struct Piece *board[8][8], int x, int y){
+}
+
+bool special_moves(struct Piece *board[8][8], int x, int y){
+}
