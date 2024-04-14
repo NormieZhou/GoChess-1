d0777886cc38daeb93b4d555388d1dd14a02d3cc
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-14 16:13:38 -0700

    AI.c

---
+++ AI.c
@@ -0,0 +1,13 @@
+#include "AI.h"
+
+struct AlphaBetaResult alpha_beta_search(struct Game *game, int depth, int alpha, int beta, struct Player *player) {
+   
+}
+
+int evaluate_position(struct Game *game, struct Player *player) {
+    
+}
+
+struct Move *generate_legal_moves(struct Game *game, struct Player *player, int *num_moves) {
+    
+}
