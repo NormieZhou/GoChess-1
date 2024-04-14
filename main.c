735803fc7355697f84fd578e044222a7a9192061
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-14 16:16:37 -0700

    main.c

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "AI.h"
+#include "Board.h"
+#include "CheckMove.h"
+#include "EndGame.h"
+#include "Log.h"
 
-int main(int x, char** y){
-    printf("Hello, from GoChess!\n");
+int main(void) {
+    //initializations
+    struct Board board;
+    init_board(&board);
+    
+    struct Player white_player = {'W', false};
+    struct Player black_player = {'B', false};
+    
+    struct Game game = {board, &white_player, NULL};
+
+    //game loop
+    while (!is_game_over(&game)) {
+        // game logic
+    }
+    
+    return 0;
 }
