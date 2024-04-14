b2bcaa4ccb16a07f45167e578812bc5ff3e975f3
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-14 16:15:19 -0700

    EndGame.c

---
+++ EndGame.c
@@ -0,0 +1,10 @@
+#include "EndGame.h"
+
+int is_in_check(struct Game *game, struct Player *player) {
+}
+
+int is_checkmate(struct Game *game, struct Player *player) {
+}
+
+int is_stalemate(struct Game *game, struct Player *player) {
+}
