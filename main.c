000a52708c5dda16f15c494fa5f2b1912f212f96
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-19 21:30:32 -0700

    main.c

--- main.c
+++ main.c
@@ -6,20 +6,53 @@
 #include "EndGame.h"
 #include "Log.h"
 
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "AI.h"
+#include "Board.h"
+#include "CheckMove.h"
+#include "EndGame.h"
+#include "Log.h"
+#include "Player.h"
+
+// Function to convert a letter to its corresponding index (0-7)
+int letter_to_index(char letter) {
+    return tolower(letter) - 'a';
+}
+
+struct Move get_user_move() {
+    struct Move move;
+
+    // Get starting and ending position from user
+    printf("Enter the starting and ending  position (format: a2 a3): ");
+    char start_letter, end_letter;
+    int start_number, end_number;
+    scanf(" %c%d %c%d", &start_letter, &start_number, &end_letter, &end_number);
+    move.start.x = letter_to_index(start_letter);
+    move.start.y = 8-start_number;
+
+    move.end.x = letter_to_index(end_letter);
+    move.end.y = 8-end_number;
+
+    return move;
+}
+
 int main(void) {
-    //initializations
+    //board initialization
     struct Board board;
     init_board(&board);
-    
-    struct Player white_player = {'W', false};
-    struct Player black_player = {'B', false};
-    
-    struct Game game = {board, &white_player, NULL};
 
-    //game loop
-    while (!is_game_over(&game)) {
-        // game logic
+    // player initialization
+    struct Player* white_player = create_player('W', false);
+    struct Player* black_player = create_player('B', false);
+
+    print_board(&board);
+    struct Move m = get_user_move();
+    while (true) {
+        move_piece(&board, &m);
+        print_board(&board);
     }
-    
     return 0;
 }
