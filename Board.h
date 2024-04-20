ab993036ce0e46fd96849ae047a61b167a436e65
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-19 21:53:15 -0700

    Board.h

--- Board.h
+++ Board.h
@@ -1,16 +1,13 @@
-#ifndef BOARD_H
-#define BOARD_H
-
-#include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "Player.h"
-
-
 // Piece type enumeration definition
 enum PieceType {
     KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN
+};
+
+struct Piece {
+    // Piece type
+    enum PieceType type;
+    // Owning player
+    enum PlayerColor color;
 };
 
 struct Position {
@@ -29,18 +26,27 @@ struct Move {
 
 struct Board {
     // Two-dimensional array representing pieces on each position of the board
-    struct Piece *squares[8][8];
+    struct Piece* squares[8][8];
+    // Flags to track whether castling is possible for each player
+    bool white_can_castle_kingside;
+    bool white_can_castle_queenside;
+    bool black_can_castle_kingside;
+    bool black_can_castle_queenside;
 };
 
 // Function to initialize the board
-void init_board(struct Board *board);
+void init_board(struct Board* board);
 
 // Function to copy the board
-void copy_board(struct Board *src, struct Board *dest);
+void copy_board(struct Board* src, struct Board* dest);
 
+// Function to print the current board position
 void print_board(struct Board* board);
 
+// Function to create a piece with the given type and color
 struct Piece* create_piece(enum PieceType type, enum PlayerColor color);
 
+// Function to move a piece on the board based on user input
+bool move_piece(struct Board* board, struct Move* move);
 
 #endif /* BOARD_H */
