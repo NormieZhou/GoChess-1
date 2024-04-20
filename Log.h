b1504bb89df8f1f6e1703589124dfd7c376253ca
15921977326 <58652296+15921977326@users.noreply.github.com>
2024-04-19 21:49:12 -0700

    Log.h

--- Log.h
+++ Log.h
@@ -22,7 +22,6 @@ struct BoardList {
 // Function declarations for logging moves
 void init_board_list(struct BoardList *list);
 void insert_board(struct BoardList *list, struct Board *board);
-void delete_board(struct BoardList *list);
 void clear_board_list(struct BoardList *list);
 void undo_move(struct Game *game, struct BoardList *list);
 
