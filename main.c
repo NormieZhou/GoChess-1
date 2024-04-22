fb60f9425e778a60408cf46e0c5aea1a0a15b340
NormieZhou <143080804+NormieZhou@users.noreply.github.com>
2024-04-21 22:23:23 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include "AI.h"
 #include "Board.h"
+#include "Game.h"
 #include "CheckMove.h"
 #include "EndGame.h"
 #include "Log.h"
@@ -32,19 +33,101 @@ struct Move get_user_move() {
 }
 
 int main(void) {
-    // board initialization
-    struct Board board;
-    init_board(&board);
+    // 提示用户选择游戏模式
+    printf("Welcome to Chess Game!\n");
+    printf("Please choose the game mode:\n");
+    printf("1. Human vs Human\n");
+    printf("2. Human vs AI\n");
+    printf("3. AI vs AI\n");
 
-    // player initialization
-    struct Player* white_player = create_player('W', false);
-    struct Player* black_player = create_player('B', false);
+    int choice;
+    printf("Enter your choice (1-3): ");
+    scanf("%d", &choice);
 
-    print_board(&board);
-    while (true) {
-        struct Move m = get_user_move();
-        move_piece(&board, &m);
-        print_board(&board);
+    struct Game game;
+    struct Player player1, player2;
+    struct Move move;
+    struct Piece piece;
+
+    // 初始化玩家信息
+    // 玩家1为白色，玩家2为黑色
+    player1.color = WHITE;
+    player2.color = BLACK;
+
+    // 根据用户选择初始化游戏
+    switch (choice) {
+    case 1:
+        // 人人模式
+        printf("Human vs Human mode selected.\n");
+        player1.isComputer = false;
+        player2.isComputer = false;
+        break;
+    case 2:
+        // 人机模式
+        printf("Human vs AI mode selected.\n");
+        player1.isComputer = false;
+        player2.isComputer = true;
+        break;
+    case 3:
+        // 机机模式
+        printf("AI vs AI mode selected.\n");
+        player1.isComputer = true;
+        player2.isComputer = true;
+        break;
+    default:
+        printf("Invalid choice. Exiting...\n");
+        return 1;
     }
-    return 0;
+    init_game(&game, player1, player2);
+    while (true) {
+        printf("It is player1's turn:\n");
+        print_board(&game.board);
+        if (player1.isComputer) {
+
+        }
+        else {
+            do {
+                move = get_user_move();
+                piece = *game.board.squares[move.start.y][move.start.x];
+            } while (!is_legal_move(&game.board, &piece, &move) || piece.color!=player1.color);
+            move_piece(&game.board, &move);
+            insert_board(&game.log, &game.board);
+        }
+        /*
+        if (is_checkmate(&game,&player2)) {
+            printf("Player1 Win!\n");
+            break;
+        }
+        else if (is_stalemate(&game, &player2)) {
+            printf("Stalemate!\n");
+            break;
+        }
+        */
+        switch_players(&game);
+        printf("It is player2's turn:\n");
+        print_board(&game.board);
+        if (player2.isComputer) {
+
+        }
+        else {
+            do {
+                move = get_user_move();
+                piece = *game.board.squares[move.start.y][move.start.x];
+            } while (!is_legal_move(&game.board, &piece, &move) || piece.color != player2.color);
+            move_piece(&game.board, &move);
+            insert_board(&game.log, &game.board);
+        }
+        /*
+        if (is_checkmate(&game, &player1)) {
+            printf("Player1 Win!");
+            break;
+        }
+        else if (is_stalemate(&game, &player1)) {
+            printf("Stalemate!");
+            break;
+        }
+        */
+        switch_players(&game);
+
+    }
 }
