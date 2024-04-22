37b4c67565bcb65f60af04e154b84c5227b89f03
NormieZhou <143080804+NormieZhou@users.noreply.github.com>
2024-04-21 22:21:16 -0700

    Update Log.h

--- Log.h
+++ Log.h
@@ -23,6 +23,6 @@ struct BoardList {
 void init_board_list(struct BoardList *list);
 void insert_board(struct BoardList *list, struct Board *board);
 void clear_board_list(struct BoardList *list);
-void undo_move(struct Game *game, struct BoardList *list);
+void undo_move(struct BoardList *list);
 
 #endif /* LOG_H */
